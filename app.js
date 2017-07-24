329e833287d25af3cacdfb391b4c52da63470475
Mario Moreno <mario.moreno.a93@gmail.com>
2017-07-23 21:34:54 -0500

    Bot facebook messenger with artificial intelligence (AI)
    Bot facebook messenger with artificial intelligence (AI)

--- app.js
+++ app.js
@@ -5,39 +5,34 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const fs = require('fs')
 const Promise = require("bluebird");
+require('dotenv').config()
 
-// Declare connection database 
-var getSqlConnection = require('./databaseConnection');
-
-// Declare token facebook 
-const APP_TOKEN = 'EAAEi5xta9rUBAMZAoklTSPikydMZBSADVZBvVAkwP5jvhxuQwt0NMbnrGXoragec3xcCrZAMRB5OPR18vht7igKB21PZCSYZCVsk0IM0JTZCvwGAEWjZCNKCIj7D2sZChSZB1uiH3IHmcF2pmMc7g6pGYgYMZASJ34R9cm7HcUuLmp7LAZDZD';
+// Declare connection database
+var getSqlConnection = require('./app/database/config.js');
 
 var app = express()
 
-// Declare folder path 
-const folderPath = __dirname + '/app'
+// Declare folder path
+const folderPath = __dirname + '/src'
 
 // Parse incoming requests
 app.use(bodyParser.json())
-
-// Declare port 
-var PORT = process.env.PORT || 3000;
 
 // Mount your static paths
 // Renders your image, title, paragraph and index.html
 app.use(express.static(folderPath))
 
 // Start the server.
-app.listen(PORT,function(){
-	console.log('Listening localhost:3000')
+app.listen(process.env.PORT,function(){
+	console.log('Listening localhost in port: ' + process.env.PORT);
 })
 
-// Read file index and send 
+// Read file index and send
 app.get('/',function(req, res){
 	res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-// Request with method get to webhook 
+// Request with method get to webhook
 app.get('/webhook',function(req, res){
 	if(req.query['hub.verify_token'] === 'hello_token'){
 		res.send(req.query['hub.challenge'])
@@ -49,11 +44,10 @@ app.get('/webhook',function(req, res){
 // Request with method post to webhook
 app.post('/webhook',function(req, res){
 	var data = req.body
-
 	if(data.object == 'page'){
 		data.entry.forEach(function(pageEntry){
 			pageEntry.messaging.forEach(function(messagingEvent){
-				if(messagingEvent.message){					
+				if(messagingEvent.message){
 					getMessage(messagingEvent)
 				}
 			})
@@ -66,7 +60,6 @@ app.post('/webhook',function(req, res){
 function getMessage(messagingEvent){
 	var senderID = messagingEvent.sender.id
 	var messageText = messagingEvent.message.text
-	
 	evaluateTextMessage(senderID, messageText)
 }
 
@@ -91,7 +84,6 @@ function SendTextMessage(senderID, messageText){
 			text: messageText
 		}
 	}
-
 	callSendApi(messageData)
 }
 
@@ -99,7 +91,7 @@ function SendTextMessage(senderID, messageText){
 function callSendApi(messageData){
 	request({
 		uri: 'https://graph.facebook.com/v2.9/me/messages',
-		qs: {access_token: APP_TOKEN},
+		qs: {access_token: process.env.APP_TOKEN},
 		method: 'POST',
 		json: messageData
 	},function(error, response, data){
